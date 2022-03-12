d774a9b70c3d0c46ec8025d21617b53511306d2a
EmaLau <emanuelelauro69@gmail.com>
2022-03-12 18:57:39 +0100

    .

--- Python/messaggi.py
+++ Python/messaggi.py
@@ -1,6 +1,10 @@
+from signal import pause
+
+
 print("Inserisci la parola da riptere")
 a=input()
 print("Inserisci il numero per quante volte deve ripetere: "+a)
 b=int(input())
 for i in range(b):
     print(a+" "+str(i+1))
+pause
