33c420f9f14c0489d99fe3cd9a85b7963c45ee5e
EmaLau <emanuelelauro69@gmail.com>
2022-09-28 08:47:13 +0200

    .

--- Veicoli/src/main/java/com/mycompany/veicoli/Veicolo.java
+++ Veicoli/src/main/java/com/mycompany/veicoli/Veicolo.java
@@ -1,10 +1,9 @@
 package com.mycompany.veicoli;
 
-
 public class Veicolo {
+
     //Attributes
     private String targa;
-    private String n_telaio;
     private boolean acceso;
     private int livello_carburante;
     private int riserva;
@@ -14,5 +13,76 @@ public class Veicolo {
     private String marca;
     private String modello;
     //Metods
-    
+
+    public void set_targa() {
+
+    }
+
+    public String get_targa() {
+        return targa;
+    }
+
+    public void set_acceso() {
+
+    }
+
+    public boolean get_acceso() {
+        return acceso;
+    }
+
+    public void set_livello_carburante() {
+
+    }
+
+    public int get_livello_carburante() {
+        return livello_carburante;
+    }
+
+    public void set_riserva() {
+
+    }
+
+    public int get_riserva() {
+        return riserva;
+    }
+
+    public void set_velocita() {
+
+    }
+
+    public int get_velocita() {
+        return velocita;
+    }
+
+    public void set_marcia() {
+
+    }
+
+    public int get_marcia() {
+        return marcia;
+    }
+
+    public void set_cilindrata() {
+
+    }
+
+    public int get_cilindrata() {
+        return cilindrata;
+    }
+
+    public void set_marca() {
+
+    }
+
+    public String get_marca() {
+        return marca;
+    }
+
+    public void set_modello() {
+
+    }
+
+    public String get_modello() {
+        return modello;
+    }
 }
--- Veicoli/src/main/java/com/mycompany/veicoli/Veicolo_Terrestre.java
+++ Veicoli/src/main/java/com/mycompany/veicoli/Veicolo_Terrestre.java
@@ -1,11 +1,13 @@
 package com.mycompany.veicoli;
 
-
 public class Veicolo_Terrestre {
+
     //Attributes
     //private int n_airbag;
     //private int n_porte; da mettere su auto
     private String tipo_carburante;
     private boolean esp;
     private int dimensione_pneumatici;
+    private String n_telaio;
+
 }
