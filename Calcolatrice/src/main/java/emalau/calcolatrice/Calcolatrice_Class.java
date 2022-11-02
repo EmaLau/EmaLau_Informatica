558bab77120b7c79faa026918fffd6a7be90a45b
EmaLau <emanuelelauro69@gmail.com>
2022-11-02 10:03:38 +0100

    .

---
+++ Calcolatrice/src/main/java/emalau/calcolatrice/Calcolatrice_Class.java
@@ -0,0 +1,18 @@
+package emalau.calcolatrice;
+
+public class Calcolatrice_Class {
+    private double n1;
+    private double n2;
+    private String operando;
+    private double tot;
+    
+    public void set_n1(double n1){
+        this.n1 = n1;
+    }
+    
+    public void set_n2(double n2){
+        this.n2 = n2;
+    }
+    
+    
+}
---
+++ Somma_Di_2_Numeri/src/main/java/emalau/somma_di_2_numeri/Somma_Di_2_Numeri_Class.java
@@ -0,0 +1,60 @@
+package emalau.somma_di_2_numeri;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author emalau
+ */
+public class Somma_Di_2_Numeri_Class {
+
+    private double n1;
+    private double n2;
+
+    public void get_n1(double n1) {
+        this.n1 = n1;
+    }
+
+    public void get_n2(double n2) {
+        this.n2 = n2;
+    }
+
+    public double set_n1() {
+        return this.n1;
+    }
+
+    public double set_n2() {
+        return this.n2;
+    }
+
+    public void get_Addizione() {
+    }
+
+    public double set_Addizione() {
+        return this.n1 + this.n2;
+    }
+
+    public void get_Sottrazione() {
+    }
+
+    public double set_Sottrazione() {
+        return this.n1 - this.n2;
+    }
+
+    public void get_Divisione() {
+    }
+
+    public double set_Divisione() {
+        return this.n1 / this.n2;
+    }
+
+    public void get_Moltiplicazione() {
+    }
+
+    public double set_Moltiplicazione() {
+        return this.n1 * this.n2;
+    }
+
+}
