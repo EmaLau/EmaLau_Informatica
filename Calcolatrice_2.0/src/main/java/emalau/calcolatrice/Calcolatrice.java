cd25925b11bc42ce2181aaeff05758610225cd22
EmaLau <emalau@MacBook-Air-di-EmaLau.local>
2022-11-28 12:16:39 +0100

    .

---
+++ Calcolatrice_2.0/src/main/java/emalau/calcolatrice/Calcolatrice.java
@@ -0,0 +1,9 @@
+package emalau.calcolatrice;
+
+public class Calcolatrice {
+
+    public static void main(String[] args) {
+        Calcolatrice_GUI Calc_GUI = new Calcolatrice_GUI();
+        Calc_GUI.setVisible(true);
+    }
+}
