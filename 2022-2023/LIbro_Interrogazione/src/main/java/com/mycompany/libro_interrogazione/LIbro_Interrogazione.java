7af0d6fc44b66c77000de24e9ace23cefe088f17
EmaLau <emanuelelauro69@gmail.com>
2023-01-26 10:04:24 +0100

    Interrogazione

--- 2022-2023/LIbro_Interrogazione/src/main/java/com/mycompany/libro_interrogazione/LIbro_Interrogazione.java
+++ 2022-2023/LIbro_Interrogazione/src/main/java/com/mycompany/libro_interrogazione/LIbro_Interrogazione.java
@@ -14,7 +14,6 @@ public class LIbro_Interrogazione {
         Libro_Class Libro = new Libro_Class(titolo, autore);
         Libro_Class Libro_2 = new Libro_Class(titolo, autore, isbn);
         Libro_Figlio_Class Libro_3 = new Libro_Figlio_Class(genere, n_page);
-
         
         System.out.print(Libro_3.get_genere());
     }
