dafc4f97282c7a24866f26196281714e31555519
Emalau <emanuelelauro69@gmail.com>
2023-02-14 22:42:18 +0100

    modified extends

--- 2022-2023/Libreria_eredita/src/main/java/emalau/libreria_eredita/Libary_JFrame.java
+++ 2022-2023/Libreria_eredita/src/main/java/emalau/libreria_eredita/Libary_JFrame.java
@@ -26,21 +26,154 @@ public class Libary_JFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jtl_titolo = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jtl_autore = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        jtl_editore = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jtl_isbn = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        jtl_pagine = new javax.swing.JTextField();
+        btn_add = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Titolo:");
+
+        jtl_titolo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtl_titoloActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Autore:");
+
+        jtl_autore.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtl_autoreActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Editore:");
+
+        jtl_editore.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtl_editoreActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("ISBN:");
+
+        jtl_isbn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtl_isbnActionPerformed(evt);
+            }
+        });
+
+        jLabel5.setText("Pagine:");
+
+        jtl_pagine.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jtl_pagineActionPerformed(evt);
+            }
+        });
+
+        btn_add.setText("ADD");
+        btn_add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_addActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jtl_titolo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jtl_autore, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addGap(18, 18, 18)
+                        .addComponent(jtl_editore, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jtl_isbn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addGap(18, 18, 18)
+                        .addComponent(jtl_pagine, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(272, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btn_add, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jtl_titolo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jtl_autore, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jtl_editore, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(jtl_isbn, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jtl_pagine, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 83, Short.MAX_VALUE)
+                .addComponent(btn_add)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jtl_titoloActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtl_titoloActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtl_titoloActionPerformed
+
+    private void jtl_autoreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtl_autoreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtl_autoreActionPerformed
+
+    private void jtl_editoreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtl_editoreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtl_editoreActionPerformed
+
+    private void jtl_isbnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtl_isbnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtl_isbnActionPerformed
+
+    private void jtl_pagineActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jtl_pagineActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jtl_pagineActionPerformed
+
+    private void btn_addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btn_addActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +211,16 @@ public class Libary_JFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_add;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JTextField jtl_autore;
+    private javax.swing.JTextField jtl_editore;
+    private javax.swing.JTextField jtl_isbn;
+    private javax.swing.JTextField jtl_pagine;
+    private javax.swing.JTextField jtl_titolo;
     // End of variables declaration//GEN-END:variables
 }
--- 2022-2023/Libreria_eredita/src/main/java/emalau/libreria_eredita/Mensola_Class.java
+++ 2022-2023/Libreria_eredita/src/main/java/emalau/libreria_eredita/Mensola_Class.java
@@ -4,6 +4,6 @@ package emalau.libreria_eredita;
  *
  * @author emalau
  */
-public class Mensola_Class {
+public class Mensola_Class extends Book_Class {
     
 }
