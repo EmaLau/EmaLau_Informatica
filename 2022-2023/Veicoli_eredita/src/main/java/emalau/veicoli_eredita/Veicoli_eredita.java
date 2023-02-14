a37fb196e5a7c871fcd9fe839a3190d1c01005f1
EmaLau <emanuelelauro69@gmail.com>
2023-02-14 09:30:04 +0100

    Created Veicoli_eredita

---
+++ 2022-2023/Veicoli_eredita/src/main/java/emalau/veicoli_eredita/Veicoli_eredita.java
@@ -0,0 +1,12 @@
+package emalau.veicoli_eredita;
+
+/**
+ *
+ * @author emalau
+ */
+public class Veicoli_eredita {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
