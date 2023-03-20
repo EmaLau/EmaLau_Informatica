51d2a1ad83b552f089bb8251572ff8aea8ed8f34
Emalau <emanuelelauro69@gmail.com>
2023-03-20 23:29:09 +0100

    Created a Scientific Calculator

---
+++ 2022-2023/Calcolatrice_Scientifica/src/main/java/emalau/calcolatrice_scientifica/Calcolatrice_Scientifica.java
@@ -0,0 +1,12 @@
+package emalau.calcolatrice_scientifica;
+
+/**
+ *
+ * @author emala
+ */
+public class Calcolatrice_Scientifica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
