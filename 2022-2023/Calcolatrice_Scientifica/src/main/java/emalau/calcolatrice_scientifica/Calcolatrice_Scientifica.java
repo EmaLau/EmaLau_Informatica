3f9cedeae6f519f5b61a0fd3d3c19b80375843ee
EmaLau <emanuelelauro69@gmail.com>
2023-03-28 07:42:22 +0200

    Calcolatrice Scientifica Working

--- 2022-2023/Calcolatrice_Scientifica/src/main/java/emalau/calcolatrice_scientifica/Calcolatrice_Scientifica.java
+++ 2022-2023/Calcolatrice_Scientifica/src/main/java/emalau/calcolatrice_scientifica/Calcolatrice_Scientifica.java
@@ -1,12 +1,9 @@
 package emalau.calcolatrice_scientifica;
 
-/**
- *
- * @author emala
- */
 public class Calcolatrice_Scientifica {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CalcolatriceScientifica_GUI Calc_GUI = new CalcolatriceScientifica_GUI();
+        Calc_GUI.setVisible(true);
     }
 }
