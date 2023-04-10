f8d35369ea2da97b08d1de12e7564d35e7fe25ef
EmaLau <emanuelelauro69@gmail.com>
2023-04-11 00:35:09 +0200

    Deletd Old Scientific Calculator

--- 2022-2023/Calcolatrice_Scientifica/src/main/java/emalau/calcolatrice_scientifica/Calcolatrice_Scientifica.java
+++
@@ -1,9 +0,0 @@
-package emalau.calcolatrice_scientifica;
-
-public class Calcolatrice_Scientifica {
-
-    public static void main(String[] args) {
-        CalcolatriceScientifica_GUI Calc_GUI = new CalcolatriceScientifica_GUI();
-        Calc_GUI.setVisible(true);
-    }
-}
