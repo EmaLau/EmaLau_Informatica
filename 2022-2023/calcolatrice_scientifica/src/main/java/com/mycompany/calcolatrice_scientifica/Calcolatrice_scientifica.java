6c3debe3e026b78f9b5441bcf530e86b3e36a2e1
EmaLau <emalau@EmaLaus-Air.local>
2023-04-11 23:08:54 +0200

    Calcolatrice_scientifica created

---
+++ 2022-2023/calcolatrice_scientifica/src/main/java/com/mycompany/calcolatrice_scientifica/Calcolatrice_scientifica.java
@@ -0,0 +1,13 @@
+package com.mycompany.calcolatrice_scientifica;
+
+/**
+ *
+ * @author emalau
+ */
+public class Calcolatrice_scientifica {
+
+    public static void main(String[] args) {
+        Calcolatrice_base CB = new Calcolatrice_base();
+        CB.setVisible(true);
+    }
+}                                                                                                                                                   
